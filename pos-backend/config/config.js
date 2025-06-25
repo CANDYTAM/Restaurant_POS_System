06e86bd9cbf2fd01ec7c60cab7f05f67ad0c02c5
Candy Tam <99579195+CANDYTAM@users.noreply.github.com>
2025-06-26 04:27:27 +0800

    change

--- pos-backend/config/config.js
+++ pos-backend/config/config.js
@@ -1,8 +1,9 @@
 require("dotenv").config();
 
 const config = Object.freeze({
-    port: process.env.PORT || 3000,
-    databaseURI: process.env.MONGODB_URI || "mongodb://localhost:27017/pos-db",
+    port: process.env.PORT || 8000,
+    // databaseURI: process.env.MONGODB_URI || "mongodb://localhost:27017/pos-db",
+    databaseURI: process.env.MONGODB_URI || "mongodb+srv://admin:admin123@cluster0.0tdi1yy.mongodb.net/pos-system?retryWrites=true&w=majority",
     nodeEnv : process.env.NODE_ENV || "development",
     accessTokenSecret: process.env.JWT_SECRET,
     razorpayKeyId: process.env.RAZORPAY_KEY_ID,
